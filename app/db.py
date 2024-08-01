491700b442504003d18dbedbb42b9692eace2f4d
vasya <vasya3222@inbox.ru>
2024-08-01 18:36:51 +0300

    Get db conn: engine and session

---
+++ app/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+from app.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    pool_size=10,
+    future=True
+)
+async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
