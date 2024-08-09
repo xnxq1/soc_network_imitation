68713a6e2609d524ea9e032b587a4cac5aad52ce
xnxq1 <vasya3222@inbox.ru>
2024-08-09 18:46:04 +0300

    Edit hierarchy custom errors

--- app/users/errors.py
+++ app/users/errors.py
@@ -1,37 +1,40 @@
 from fastapi import HTTPException
 
 
-class ThereIsAUserError(Exception):
+class BaseCustomError(Exception):
+    message = "Unknown"
+    status_code = 500
+    def __init__(self):
+        raise HTTPException(status_code=self.status_code, detail=self.message)
+
+
+class ThereIsAUserError(BaseCustomError):
     message = "Такой пользователь уже есть"
     status_code = 409
 
-    def __init__(self):
-        raise HTTPException(status_code=self.status_code, detail=self.message)
 
 
-class ValidationError(Exception):
+
+class ValidationError(BaseCustomError):
     message = "Не правильно введены данные"
     status_code = 409
 
-    def __init__(self):
-        raise HTTPException(status_code=self.status_code, detail=self.message)
 
 
-class WrongDataForAuthError(Exception):
+class WrongDataForAuthError(BaseCustomError):
     message = "Неправильно введен пароль или почта"
     status_code = 401
 
-    def __init__(self):
-        raise HTTPException(status_code=self.status_code, detail=self.message)
 
 
-class JWTCustomError(Exception):
+
+class JWTCustomError(BaseCustomError):
     message = "У вас нет jwt токена или он неправильный"
     status_code = 401
 
     def __init__(self, message: str = None, status_code: str = None):
-        msg = message if message else self.message
-        code = status_code if status_code else self.status_code
-        raise HTTPException(status_code=code, detail=msg)
+        self.message = message if message else self.message
+        self.status_code = status_code if status_code else self.status_code
+        super().__init__()
 
 
